e7c233db6623b695b6efdb105940acfda8d0b1d0
Ayushsaini03 <114101553+Ayushsaini03@users.noreply.github.com>
2025-04-09 17:58:10 +0530

    db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,6 +2,6 @@ import mongoose from "mongoose";
 
 
 export const connectDB = async ()=>{
-    await mongoose.connect('mongodb+srv://aayushsaini2003:Ayush022003@cluster0.gbpbsxw.mongodb.net/test?retryWrites=true&w=majority').then(()=> console.log("DB Connected"));
+    await mongoose.connect('mongodb+srv://aayushsaini2003:Ayush022003@cluster0.gbpbsxw.mongodb.net/test?retryWrites=true&w=majority&tls=true&tlsAllowInvalidCertificates=true').then(()=> console.log("DB Connected"));
 }
 
